e36d0a153bf00e06c0fb3f9000d50872ada18eb2
Aditya-550 <primeking2004@gmail.com>
2024-02-19 17:12:14 +0530

    Lightning Module

---
+++ distributed-training/launch-lightning.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -J machine-learing              # name of the job
+
+#SBATCH -N 1
+
+#SBATCH -p cpu                      # name of the partition: available options "cpu, hm or gpu"
+
+#SBATCH --ntasks-per-node=4         # no of processes or tasks per node
+
+#SBATCH -t 01:00:00                 # walltime in HH:MM:SS, Max value 72:00:00   gromacs/iwey76m
+
+#SBATCH --error=job_%j.err
+
+#SBATCH --output=job_%j.out
+
+source /home/apps/anaconda3/bin/activate
+conda activate machine-learning
+
+# run script from above
+srun python3 lightning.py
---
+++ distributed-training/train.py
@@ -0,0 +1,17 @@
+import os
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+import lightning as pl
+import LightningModule as L
+
+# setup data
+dataset = MNIST(os.getcwd(), download=True, transform=L.ToTensor())
+train_loader = torch.utils.data.DataLoader(dataset)
+
+# train the model (hint: here are some helpful Trainer arguments for rapid idea iteration)
+trainer = L.Trainer(limit_train_batches=100, max_epochs=1)
+trainer.fit(model=L.autoencoder, train_dataloaders=train_loader)
